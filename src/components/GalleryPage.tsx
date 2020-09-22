ee71619b59a9155e45f6cbb4649d9192c7689a36
Anders Solberg <ssredna@gmail.com>
2020-09-22 11:17:21 +0200

    RP-7: fetch poems from poetryDB and add it inn galleryitem

--- src/components/GalleryPage.tsx
+++ src/components/GalleryPage.tsx
@@ -1,10 +1,10 @@
-import React, {useState, MouseEvent, TouchEvent} from 'react';
+import React, { useState, MouseEvent, TouchEvent, useEffect } from 'react';
 import '../ColorPalette.css';
 import '../Container.css';
 import './GalleryPage.css';
 import '../General.css';
 import GalleryItem from './GalleryItem';
-import {ColorTheme} from './GalleryItem.dto';
+import { ColorTheme } from './GalleryItem.dto';
 
 interface Poem {
   title: string;
@@ -22,258 +22,13 @@ const GalleryPage = () => {
     FigureAmount.Few
   );
   const [colorTheme, setColorTheme] = useState(ColorTheme.theme1);
+  const [poems, setPoems] = useState<Poem[]>([]);
 
-  const poems: Poem[] = [
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum 2',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum 3',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuum 5',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum 7',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsu 9',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-    {
-      author: 'Shakespeare',
-      title: 'Lorem ipsuuuum',
-      lines: [
-        'From fairest creatures we desire increase,',
-        "That thereby beauty's rose might never die,",
-        'But as the riper should by time decease,',
-        'His tender heir might bear his memory:',
-        'But thou contracted to thine own bright eyes,',
-        "Feed'st thy light's flame with self-substantial fuel,",
-        'Making a famine where abundance lies,',
-        'Thy self thy foe, to thy sweet self too cruel:',
-        "Thou that art now the world's fresh ornament,",
-        'And only herald to the gaudy spring,',
-        'Within thine own bud buriest thy content,',
-        "And tender churl mak'st waste in niggarding:",
-        '  Pity the world, or else this glutton be,',
-        "  To eat the world's due, by the grave and thee.",
-      ],
-    },
-  ];
-
-  const items = [] as any;
-  for (let i = 0; i < poems.length; i++) {
-    items.push(
-      <GalleryItem
-        key={i}
-        poem={poems[i]}
-        colorTheme={colorTheme}
-        amount={figureAmount}
-        figureType={null}
-      />
-    );
-  }
+  useEffect(() => {
+    fetch('https://poetrydb.org/linecount,poemcount/8;10')
+      .then((res: Response) => res.json())
+      .then((poemData: Poem[]) => setPoems(poemData));
+  }, []);
 
   const updateFigureAmount = (
     event: MouseEvent<HTMLInputElement> | TouchEvent<HTMLInputElement>
@@ -287,7 +42,7 @@ const GalleryPage = () => {
       <h1 className="GalleryPage-title">Gallery</h1>
       <div className="GalleryPage-actions">
         <div className="dropdown">
-          <button className="dropbtn" style={{background: colorTheme.color1}}>
+          <button className="dropbtn" style={{ background: colorTheme.color1 }}>
             Theme: {colorTheme.name}
           </button>
           <div className="dropdown-content">
@@ -319,7 +74,21 @@ const GalleryPage = () => {
           <span className="GalleryPage-figure-amount"> {figureAmount}</span>
         </div>
       </div>
-      <div className="GalleryPage-container">{items}</div>
+      <div className="GalleryPage-container">
+        {poems ? (
+          poems.map((d) => (
+            <GalleryItem
+              key={d.title}
+              poem={d}
+              colorTheme={colorTheme}
+              amount={figureAmount}
+              figureType={null}
+            />
+          ))
+        ) : (
+          <p>nothing here</p>
+        )}
+      </div>
     </div>
   );
 };
